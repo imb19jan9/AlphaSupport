b9416a1a6087acddfc32c32059b73026d9b30504
imb19jan9 <imb19jan9@gmail.com>
2020-12-08 19:22:03 +0900

    mesh export function added

--- play.py
+++ play.py
@@ -7,7 +7,9 @@ from stable_baselines3 import PPO
 
 import argparse
 
-from env import Support_v0, Support_v1
+from env import Support_v0
+
+from print import extrude
 
 
 class App(tk.Tk):
@@ -40,12 +42,23 @@ class App(tk.Tk):
             self.control_panel, text="Skip", command=self.skip
         )
         self.skip_button.grid(row=0, column=3, sticky='ew')
+
+        self.print_button = ttk.Button(
+        self.control_panel, text="Print", command=self.print
+        )
+        self.print_button.grid(row=0, column=4, sticky='ew')
+
         self.control_panel.grid(row=1, column=0, sticky='esw')
-        self.control_panel.columnconfigure((0,1,2,3), weight=1)
+        self.control_panel.columnconfigure((0,1,2,3,4), weight=1)
 
         self.after(200, self.setup)
 
+        self.filenum = 0
+
     def setup(self):
+        self.filenum += 1
+        print(self.filenum)
+
         self.done = False
         self.obs = self.env.reset()
         cell_size = self.canvas.winfo_width() // self.env.board_size
@@ -78,12 +91,20 @@ class App(tk.Tk):
 
         self.obs, _, self.done, _ = self.env.step(action)
 
+        for i in range(self.env.board_size):
+            for j in range(self.env.board_size):
+                if self.env.model[i, j] == 0 and \
+                self.env.support[i, j] == 0:
+                    id = self.cells[f"({i},{j})"]
+                    self.canvas.itemconfig(id, fill="white")
+
         id = self.cells[f"({row},{action})"]
         self.canvas.itemconfig(id, fill="gray20")
 
     def on_reset(self):
         for item in self.canvas.find_all():
             self.canvas.delete(item)
+        
         self.setup()
 
     def show_pi(self):
@@ -104,7 +125,6 @@ class App(tk.Tk):
                     rgb = 255, 0, int(255 * (-2 * prob + 2))
                 else:
                     rgb = int(255 * (2 * prob)), 0, 255
-
                 hex = "#%02x%02x%02x" % (rgb[0], rgb[1], rgb[2])
                 id = self.cells[f"({row},{i})"]
                 self.canvas.itemconfig(id, fill=hex)
@@ -113,6 +133,9 @@ class App(tk.Tk):
         while(not self.done):
             self.on_next()
 
+    def print(self):
+        m = extrude(self.env.model, self.env.support)
+        m.save('mesh.stl')
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -122,9 +145,9 @@ if __name__ == "__main__":
 
     if args.env == 'v0':
         env_cls = Support_v0
-    else:
-        env_cls = Support_v1
-    env = env_cls(dataset_dir = "../size30/train/", board_size = 30)
+
+        
+    env = env_cls(dataset_dir = "../size50/test/", board_size = 50)
     
     model = PPO.load(args.name)
     app = App(env, model)
